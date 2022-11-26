3f821aeab34ed3bcde14194d0dba51fee0fb4fbd
Alex <alexgold1994@gmail.com>
2022-11-26 16:31:25 +0200

    add prettier and eslint

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,56 +1,56 @@
 export const Work = [
   {
     id: 1,
-    name: "Porfolio page",
-    description: "It is build on top of the React JS, with styledComponents.",
-    tags: ["react", "gsap", "framer-motion", "styledComponents"],
+    name: 'Porfolio page',
+    description: 'It is build on top of the React JS, with styledComponents.',
+    tags: ['react', 'gsap', 'framer-motion', 'styledComponents'],
 
-    demo: "https://github.com/alexgold1994",
-    github: "https://github.com/alexgold1994",
+    demo: 'https://github.com/alexgold1994',
+    github: 'https://github.com/alexgold1994'
   },
   {
     id: 2,
-    name: "Porfolio page",
-    description: "It is build on top of the React JS, with styledComponents.",
-    tags: ["react", "gsap", "framer-motion", "styledComponents"],
+    name: 'Porfolio page',
+    description: 'It is build on top of the React JS, with styledComponents.',
+    tags: ['react', 'gsap', 'framer-motion', 'styledComponents'],
 
-    demo: "https://github.com/alexgold1994",
-    github: "https://github.com/alexgold1994",
+    demo: 'https://github.com/alexgold1994',
+    github: 'https://github.com/alexgold1994'
   },
   {
     id: 3,
-    name: "Porfolio page",
-    description: "It is build on top of the React JS, with styledComponents.",
-    tags: ["react", "gsap", "framer-motion", "styledComponents"],
+    name: 'Porfolio page',
+    description: 'It is build on top of the React JS, with styledComponents.',
+    tags: ['react', 'gsap', 'framer-motion', 'styledComponents'],
 
-    demo: "https://github.com/alexgold1994",
-    github: "https://github.com/alexgold1994",
+    demo: 'https://github.com/alexgold1994',
+    github: 'https://github.com/alexgold1994'
   },
   {
     id: 4,
-    name: "Porfolio page",
-    description: "It is build on top of the React JS, with styledComponents.",
-    tags: ["react", "gsap", "framer-motion", "styledComponents"],
+    name: 'Porfolio page',
+    description: 'It is build on top of the React JS, with styledComponents.',
+    tags: ['react', 'gsap', 'framer-motion', 'styledComponents'],
 
-    demo: "https://github.com/alexgold1994",
-    github: "https://github.com/alexgold1994",
+    demo: 'https://github.com/alexgold1994',
+    github: 'https://github.com/alexgold1994'
   },
   {
     id: 5,
-    name: "Porfolio page",
-    description: "It is build on top of the React JS, with styledComponents.",
-    tags: ["react", "gsap", "framer-motion", "styledComponents"],
+    name: 'Porfolio page',
+    description: 'It is build on top of the React JS, with styledComponents.',
+    tags: ['react', 'gsap', 'framer-motion', 'styledComponents'],
 
-    demo: "https://github.com/alexgold1994",
-    github: "https://github.com/alexgold1994",
+    demo: 'https://github.com/alexgold1994',
+    github: 'https://github.com/alexgold1994'
   },
   {
     id: 6,
-    name: "Porfolio page",
-    description: "It is build on top of the React JS, with styledComponents.",
-    tags: ["react", "gsap", "framer-motion", "styledComponents"],
+    name: 'Porfolio page',
+    description: 'It is build on top of the React JS, with styledComponents.',
+    tags: ['react', 'gsap', 'framer-motion', 'styledComponents'],
 
-    demo: "https://github.com/alexgold1994",
-    github: "https://github.com/alexgold1994",
-  },
-];
+    demo: 'https://github.com/alexgold1994',
+    github: 'https://github.com/alexgold1994'
+  }
+]
